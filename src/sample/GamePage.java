1486772dcc1900cca19c7a4d4ca3169011ee588f
Göktuğ <gozturkcan@hotmail.com>
2019-12-20 00:22:27 +0300

    Revert "halikarnassos' feature implemented"
    This reverts commit b779af99fea2191160397841e2595343144bee9e.

--- src/sample/GamePage.java
+++ src/sample/GamePage.java
@@ -40,6 +40,10 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.lang.Math.min;
 
